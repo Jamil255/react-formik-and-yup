0b45b9502c23347e7df1c4754f16a69b723f32cd
JAMIL AFZAL <134855468+Jamil255@users.noreply.github.com>
2024-06-28 22:46:46 +0500

    Update index.jsx

--- src/schema/index.jsx
+++ src/schema/index.jsx
@@ -5,8 +5,8 @@ const signupSchema = Yup.object({
   email: Yup.string().email().required('please enter a eamil'),
   password: Yup.string().min(6).required('please enter a password'),
 
-  //   confirm_password: Yup.string
-  //     .required()
-  //     .oneOf([Yup.ref('password'), null], 'password must match'),
+    confirm_password: Yup.string
+      .required()
+      .oneOf([Yup.ref('password'), null], 'password must match'),
 })
 export default signupSchema
